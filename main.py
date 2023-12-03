bf4781020bd5c4bdf4710cd2e8804265c94860c6
andrea nisco <andreanisco@gmail.com>
2023-12-03 23:51:14 +0100

    "added menu in main.py, modified create_user function"

--- main.py
+++ main.py
@@ -1,100 +1,81 @@
-import requests
-from faker import Faker
 import os
+import requests
+import json
 
-# URL base e nome del realm di Keycloak
-KEYCLOAK_URL = 'http://0.0.0.0:8080/auth/'
-REALM_NAME = 'master'
+# Configura l'URL del server Keycloak
+KEYCLOAK_URL = "http://0.0.0.0:8080"  # Assicurati che questo indirizzo sia corretto
+KEYCLOAK_REALM = "master"
+KEYCLOAK_CLIENT_ID = "admin-cli"
 
-# Credenziali amministratore da variabili d'ambiente
-KEYCLOAK_ADMIN = os.environ['KEYCLOAK_ADMIN']
-KEYCLOAK_ADMIN_PASSWORD = os.environ['KEYCLOAK_ADMIN_PASSWORD']
+# Recupera le credenziali dell'amministratore dalle variabili d'ambiente
+KEYCLOAK_ADMIN_USER = os.environ.get("KEYCLOAK_ADMIN", "admin")  # Default a "admin" se non impostato
+KEYCLOAK_ADMIN_PASSWORD = os.environ.get("KEYCLOAK_ADMIN_PASSWORD", "password")  # Default a "password" se non impostato
 
-def create_group(access_token, group_name):
-    group_url = f"{KEYCLOAK_URL}admin/realms/{REALM_NAME}/groups"
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Content-Type': 'application/json'
-    }
-
-    # Controlla se il gruppo esiste già
-    response = requests.get(group_url, headers=headers)
-    response.raise_for_status()
-    groups = response.json()
-    for group in groups:
-        if group['name'] == group_name:
-            print(f"Group '{group_name}' already exists")
-            return group['id']
-
-    # Se il gruppo non esiste, crealo
-    group_data = {'name': group_name}
-    response = requests.post(group_url, json=group_data, headers=headers)
+# Funzione per ottenere il token di accesso
+def get_token():
     try:
+        url = f"{KEYCLOAK_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/token"
+        data = {
+            "client_id": KEYCLOAK_CLIENT_ID,
+            "username": KEYCLOAK_ADMIN_USER,
+            "password": KEYCLOAK_ADMIN_PASSWORD,
+            "grant_type": "password"
+        }
+        response = requests.post(url, data=data)
         response.raise_for_status()
-        print(f"Group '{group_name}' created successfully")
-        # Fai una richiesta aggiuntiva per ottenere l'ID del gruppo appena creato
-        response = requests.get(group_url, headers=headers)
-        response.raise_for_status()
-        groups = response.json()
-        for group in groups:
-            if group['name'] == group_name:
-                return group['id']
-    except HTTPError as e:
-        print(f"Failed to create group '{group_name}'. Error: {e.response.text}")
+        token = response.json()["access_token"]
+        return token
+    except requests.exceptions.RequestException as e:
+        print(f"Errore durante l'ottenimento del token: {e}")
         return None
-    
-def create_user(access_token, user_data):
-    user_url = f"{KEYCLOAK_URL}admin/realms/{REALM_NAME}/users"
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(user_url, json=user_data, headers=headers)
+
+# Funzione per creare un nuovo utente
+def create_user(token, username, email, password):
     try:
+        url = f"{KEYCLOAK_URL}/admin/realms/{KEYCLOAK_REALM}/users"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
+        data = {
+            "username": username,
+            "email": email,
+            "enabled": True,
+            "credentials": [{"type": "password", "value": password, "temporary": False}]
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
         response.raise_for_status()
-        return response.headers.get('Location').split('/')[-1]  # Get the ID of the created user
-    except HTTPError as e:
-        print(f"Failed to create user. Error: {e.response.text}")
+        return response.status_code
+    except requests.exceptions.RequestException as e:
+        print(f"Errore durante la creazione dell'utente: {e}")
         return None
-    
-def assign_user_to_group(access_token, user_id, group_id):
-    assign_url = f"{KEYCLOAK_URL}admin/realms/{REALM_NAME}/users/{user_id}/groups/{group_id}"
-    headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.put(assign_url, headers=headers)
-    try:
-        response.raise_for_status()
-        print(f"User {user_id} assigned to group {group_id} successfully")
-    except HTTPError as e:
-        print(f"Failed to assign user {user_id} to group {group_id}. Error: {e.response.text}")
 
+# Funzione per mostrare il menu e gestire le scelte dell'utente
+def main_menu():
+    while True:
+        print("\nMenu:")
+        print("1. Crea un nuovo utente")
+        print("0. Esci")
+        scelta = input("Inserisci la tua scelta: ")
 
-# Generatore di dati casuali
-faker = Faker()
+        if scelta == "1":
+            username = input("Inserisci il nome utente: ")
+            email = input("Inserisci l'email: ")
+            password = input("Inserisci la password: ")
+            token = get_token()
+            if token:
+                status_code = create_user(token, username, email, password)
+                if status_code == 201:
+                    print("Utente creato con successo.")
+                else:
+                    print(f"Errore nella creazione dell'utente. Codice di risposta: {status_code}")
+            else:
+                print("Impossibile ottenere il token di accesso.")
+        elif scelta == "0":
+            break
+        else:
+            print("Scelta non valida.")
 
-# Numero di utenti da generare
-num_users = int(input("Con quanti utenti vuoi popolare il database? "))
-
-# Inserimento dei nomi dei gruppi e creazione dei gruppi
-group_ids = []
-for _ in range(int(input("Quanti gruppi vuoi creare? "))):
-    group_name = input("Scrivi nome gruppo: ")
-    group_id = create_group(KEYCLOAK_ADMIN, KEYCLOAK_ADMIN_PASSWORD, group_name)
-    if group_id:
-        group_ids.append(group_id)
-
-# Creazione di utenti casuali e assegnazione a gruppi
-for _ in range(num_users):
-    username = faker.user_name()
-    email = faker.email()
-    user_data = {
-        "username": username,
-        "email": email,
-        "enabled": True,
-        "emailVerified": True
-    }
-    user_id = create_user(KEYCLOAK_ADMIN, KEYCLOAK_ADMIN_PASSWORD, user_data)
-    if user_id and group_ids:
-        assign_user_to_group(KEYCLOAK_ADMIN, KEYCLOAK_ADMIN_PASSWORD, user_id, faker.random.choice(group_ids))
-
-print("Popolamento del database completato.")
-
+# Esecuzione del menu principale
+if __name__ == "__main__":
+    main_menu()
